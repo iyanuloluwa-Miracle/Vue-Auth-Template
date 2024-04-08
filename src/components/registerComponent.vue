cc981bca55fdf15b2377a09f8462a384bc0873e2
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-04-08 14:53:03 +0100

    feat: implementation of the register route

---
+++ src/components/registerComponent.vue
@@ -0,0 +1,52 @@
+<template>
+    <form>
+      <div class="container">
+        <label for="uname"><b>FirstName</b></label>
+        <input type="text" placeholder="Enter firstname" name="uname" required />
+
+        <label for="uname"><b>Lastname</b></label>
+        <input type="text" placeholder="Enter Lastname" name="uname" required />
+  
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required />
+  
+        <button type="submit">Register</button>
+      </div>
+    </form>
+  </template>
+  
+  <script>
+    export default{
+      methods:{
+        
+      }
+    }
+  
+  </script>
+  
+  <style scoped>
+  form {
+    border: 3px solid #f1f1f1;
+  }
+  /* Full-width inputs */
+  input[type="text"],
+  input[type="password"] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+  }
+  
+  /* Set a style for all buttons */
+  button {
+    background-color: #04aa6d;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+  }
+  </style>
--- src/router/router.js
+++ src/router/router.js
@@ -1,12 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../components/homeComponent.vue'
 import Login from '../components/loginComponent.vue'
+import Register from '../components/registerComponent.vue'
+
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: Home },
     { path: '/login', component: Login },
+    { path: '/register', component: Register },
   ]
 })
 
